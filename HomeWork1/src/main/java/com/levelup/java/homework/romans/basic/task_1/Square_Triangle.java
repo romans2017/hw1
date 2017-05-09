f483d39aaeacf2b7b899589c80fef590ed610ca9
Romans <savchuk.r0@gmail.com>
2017-05-09 18:38:27 +0300

    final task 1_2 удалил лишние строки

--- HomeWork1/src/main/java/com/levelup/java/homework/romans/basic/task_1/Square_Triangle.java
+++ HomeWork1/src/main/java/com/levelup/java/homework/romans/basic/task_1/Square_Triangle.java
@@ -17,8 +17,6 @@ public class Square_Triangle {
             katet_b = in.nextDouble();
         } while (katet_b <= 0);
 
-        double P_Triangle = katet_a + katet_b + sqrt(katet_a*katet_a + katet_b*katet_b);
-        double S_Triangle = 0.5 * katet_a * katet_b;
         System.out.printf("Периметр треугольника:%s%n", katet_a + katet_b + sqrt(katet_a * katet_a + katet_b * katet_b));
         System.out.printf("Площадь треугольника:%s%n", 0.5 * katet_a * katet_b);
     }
