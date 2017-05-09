5da4699ffe6016a246ae79d457ec000386915bdf
Romans <savchuk.r0@gmail.com>
2017-05-09 18:31:52 +0300

    final task 1_2

---
+++ HomeWork1/src/main/java/com/levelup/java/homework/romans/basic/task_1/Square_Triangle.java
@@ -0,0 +1,25 @@
+package main.java.com.levelup.java.homework.romans.basic.task_1;
+
+import java.util.Scanner;
+import static java.lang.Math.abs;
+import static java.lang.Math.sqrt;
+
+public class Square_Triangle {
+    public static void main (String s[]) {
+        double katet_a, katet_b = 0.0;
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.print("Введите длину катета а (положительное число): ");
+            katet_a = in.nextDouble();
+        } while (katet_a <= 0);
+        do {
+            System.out.print("Введите длину катета b (положительное число): ");
+            katet_b = in.nextDouble();
+        } while (katet_b <= 0);
+
+        double P_Triangle = katet_a + katet_b + sqrt(katet_a*katet_a + katet_b*katet_b);
+        double S_Triangle = 0.5 * katet_a * katet_b;
+        System.out.printf("Периметр треугольника:%s%n", katet_a + katet_b + sqrt(katet_a * katet_a + katet_b * katet_b));
+        System.out.printf("Площадь треугольника:%s%n", 0.5 * katet_a * katet_b);
+    }
+}
