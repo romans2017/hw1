e45efc4b99481bfcc79c4b51194f0fc13f6f2b31
Romans <savchuk.r0@gmail.com>
2017-05-11 15:19:48 +0300

    исправление проверки ввода для задачи 1.2

--- HomeWork1/src/main/java/com/levelup/java/homework/romans/basic/task_1/Square_Triangle.java
+++ HomeWork1/src/main/java/com/levelup/java/homework/romans/basic/task_1/Square_Triangle.java
@@ -5,18 +5,21 @@ import static java.lang.Math.sqrt;
 
 public class Square_Triangle {
     public static void main (String s[]) {
-        double katet_a, katet_b;
+        double katet_a = 0, katet_b = 0;
         Scanner in = new Scanner(System.in);
         do {
             System.out.print("Введите длину катета а (положительное число): ");
-            katet_a = in.nextDouble();
+            if (in.hasNextDouble()) katet_a = in.nextDouble();
+            else in.next();
         } while (katet_a <= 0);
         do {
-            System.out.print("Введите длину катета b (положительное число): ");
-            katet_b = in.nextDouble();
+            System.out.print("Введите длину катета а (положительное число): ");
+            if (in.hasNextDouble()) katet_b = in.nextDouble();
+            else in.next();
         } while (katet_b <= 0);
+        in.close();
 
-        System.out.printf("Периметр треугольника:%s%n", katet_a + katet_b + sqrt(katet_a * katet_a + katet_b * katet_b));
-        System.out.printf("Площадь треугольника:%s%n", 0.5 * katet_a * katet_b);
+        System.out.printf("Периметр треугольника: %s%n", (float)katet_a + katet_b + sqrt(katet_a * katet_a + katet_b * katet_b));
+        System.out.printf("Площадь треугольника: %s%n", (float)0.5 * katet_a * katet_b);
     }
 }
